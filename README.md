4a5dbe972607284990723eba6ef4d3e193a46b5d
KengoWada <louiskengo3@gmail.com>
2019-02-18 17:44:27 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# NODE APP WITH GCP
+
+Deploy a demo app to Google App Engine
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+	const response = {
+		message: "We have launched",
+		code: 200
+	};
+	return res.status(200).json(response);
+});
+
+app.listen(port, () => {
+	console.log("Server is running");
+});
